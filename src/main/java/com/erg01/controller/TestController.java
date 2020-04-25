310321d2847c695475ec74b674f1b4fba2dcaecc
Mete Ergin <mete.ergin@outlook.com>
2020-04-25 18:14:01 +0300

    fragment added

--- src/main/java/com/erg01/controller/TestController.java
+++ src/main/java/com/erg01/controller/TestController.java
@@ -3,14 +3,13 @@ package com.erg01.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class HelloController {
+public class TestController {
 
-    @GetMapping({"/"})
-    public String hello(Model model) {
-        return "index";
+    @GetMapping({"test"})
+    public String test(Model model) {
+        return "views/test";
     }
 
 }
